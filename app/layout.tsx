fbd56802d723d25cf9007fc01f7db1a13823f248
Joseph Khawly <joseph.khawly@gmail.com>
2024-03-11 16:35:03 -0600

    chore: eslint config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import './layout.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Header from '@/components/Header';
-import Footer from "@/components/Footer";
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
   title: 'Support Local Akron - Show Your Love ❤️',
-  description: 'A place to find out how to help our local artists, musicians, and businesses.',
+  description:
+    'A place to find out how to help our local artists, musicians, and businesses.',
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,33 @@
+function Hero() {
+  return (
+    <div id="#top" className="cover animated">
+      {/* <Img
+              title="Cover image"
+              alt="Air Balloon in Nature"
+              fluid={coverphoto.childImageSharp.fluid}
+              style={{
+                position: 'relative',
+                left: 0,
+                top: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            /> */}
+      <div className="overlay">
+        <div className="center">
+          <h1 className="name">
+            <b>SUPPORT LOCAL AKRON</b>
+          </h1>
+          <h5 className="greetings">
+            Show your love to our amazing local artists, musicians, and
+            businesses
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function Page() {
   return <h1>Hello, Next.js!</h1>;
 }
