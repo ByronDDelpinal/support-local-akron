147542f54d7398588a0c1429fdb42736071e02a5
Joseph Khawly <joseph.khawly@gmail.com>
2024-03-12 14:14:50 -0600

    feat: add filtering and sorting to list page

--- app/businesses/page.tsx
+++ app/businesses/page.tsx
@@ -1,19 +1,121 @@
+'use client';
 import data from '@/data.json';
 import { BusinessCard } from '@/components/BusinessCard';
+import { useState } from 'react';
 
 export default function Businesses() {
+  const getUniqueCategories = () => {
+    const allCategories = Array.from(data.businesses).map(
+      (business) => business.category
+    );
+
+    allCategories.unshift('all');
+
+    const uniqueCategories = allCategories.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+
+    const uniqueObjects = uniqueCategories.map((category) => ({
+      name: category,
+      selected: category === 'all' ? true : false,
+    }));
+
+    return uniqueObjects;
+  };
+
+  const setCategoryToActive = (categoryName: string) => {
+    const newCategories = Array.from(categories);
+
+    newCategories.map((newCategory) => {
+      newCategory.selected = newCategory.name === categoryName;
+      return newCategory;
+    });
+
+    setCategories(newCategories);
+  };
+
+  const filterBusinesses = (categoryName: string) => {
+    // Handle "All" separately since it's a reset
+    if (categoryName === 'all') {
+      // This goes back to the default
+      setCategoryToActive(categoryName);
+      setBusinesses(data.businesses);
+      return;
+    }
+
+    const allBusinesses = Array.from(data.businesses);
+
+    const filteredBusinesses = allBusinesses.filter(
+      (business) =>
+        business.category.toLowerCase() === categoryName.toLowerCase()
+    );
+
+    setCategoryToActive(categoryName);
+    setBusinesses(filteredBusinesses);
+  };
+
+  const sortBusinesses = (direction: string) => {
+    if (!direction) {
+      // This goes back to the default
+      setBusinesses(data.businesses);
+      return;
+    }
+
+    const sortedBusinesses = Array.from(businesses).sort((a, b) => {
+      const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+      const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+
+      if (direction === 'asc') {
+        if (nameA < nameB) return -1;
+        if (nameA > nameB) return 1;
+
+        return 0;
+      }
+
+      if (nameA < nameB) return 1;
+      if (nameA > nameB) return -1;
+
+      return 0;
+    });
+
+    setBusinesses(sortedBusinesses);
+  };
+
+  const [businesses, setBusinesses] = useState(data.businesses);
+  const [categories, setCategories] = useState(getUniqueCategories());
+
   return (
     <div className="business-index-page">
       <div className="container">
-        <div className="text-center pad-20">
-          <h2 className="section-title">{data.businesses.length} Local Listings</h2>
+        <div className="pad-20 text-center">
+          <h2 className="section-title">{businesses.length} Local Listings</h2>
+        </div>
+        <div className="facets">
+          {categories.map((category) => (
+            <button
+              key={category.name}
+              className={`facet${category.selected ? ' selected' : ''}`}
+              onClick={() => filterBusinesses(category.name)}
+            >
+              {category.name}
+            </button>
+          ))}
+          <select
+            className="facet"
+            id="sort-by"
+            onChange={(event) => sortBusinesses(event.target.value)}
+          >
+            <option value="">Sort By ▼</option>
+            <option value="asc">Sort: A-Z</option>
+            <option value="desc">Sort: Z-A</option>
+          </select>
         </div>
         <ul className="article-list row">
-          {data.businesses.map(business => {
+          {businesses.map((business) => {
             return (
               <li
                 data-type={business.type}
-                key={business.urlName}
+                key={business.slug}
                 className="col-lg-6"
               >
                 <BusinessCard business={business} />
--- components/BusinessCard.tsx
+++ components/BusinessCard.tsx
@@ -1,19 +1,24 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export function BusinessCard({ business }: any) {
   const MAX_LENGTH_TITLE = 80;
 
   return (
-    <article className="blog-listing" key={business.urlName}>
+    <article className="blog-listing" key={business.slug}>
       <div className="entry-meta-content">
         <div className="entry-media">
-          <Link href={business.urlName}>
-            <Image src={business.image} alt={`${business.name} logo`} width={500} height={200} />
+          <Link href={business.slug}>
+            <Image
+              src={business.image}
+              alt={`${business.name} logo`}
+              width={500}
+              height={200}
+            />
           </Link>
         </div>
         <h2 className="entry-title">
-          <Link href={business.urlName}>
+          <Link href={business.slug}>
             {' '}
             {business.name > MAX_LENGTH_TITLE
               ? business.name
@@ -21,13 +26,11 @@ export function BusinessCard({ business }: any) {
           </Link>
         </h2>
         <p className="business-type">{business.type}</p>
-        <div className="entry-content">
-          {business.supportSummary}
-        </div>
+        <div className="entry-content">{business.supportSummary}</div>
       </div>
 
       <div className="entry-content-bottom">
-        <Link href={business.urlName} className="entry-read-more">
+        <Link href={business.slug} className="entry-read-more">
           <span />
           Support
         </Link>
